1059e0666e6a1ee2656eaff3565e6b7e3db2c48d
DPellone <DPellone@users.noreply.github.com>
2017-09-29 11:56:11 +0200

    Intercept per la corrispondenza nome utente SP-IdP (#7)

---
+++ SHIB-AP-Intercept/src/main/java/net/polito/pellone/APVerifyId.java
@@ -0,0 +1,53 @@
+package net.polito.pellone;
+
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.opensaml.saml.saml2.core.AuthnRequest;
+import org.opensaml.saml.saml2.core.SubjectConfirmation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.shibboleth.idp.authn.context.SubjectContext;
+import net.shibboleth.idp.profile.interceptor.ProfileInterceptorFlowDescriptor;
+
+
+/**
+ * 
+ * 
+ * @author Daniele Pellone
+ *
+ */
+public class APVerifyId extends ProfileInterceptorFlowDescriptor {
+	
+	
+	public APVerifyId() {
+			super();
+			this.setId("APVerifyId");
+		}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public boolean apply(ProfileRequestContext input){
+		
+		Logger log = LoggerFactory.getLogger(APVerifyId.class);
+		String requestedId, authenticatedId;
+		
+		Object messageObj = input.getInboundMessageContext().getMessage();
+		if(messageObj instanceof AuthnRequest){
+			AuthnRequest message = (AuthnRequest)messageObj;
+			SubjectConfirmation subj =  message.getSubject().getSubjectConfirmations().get(0);
+			requestedId = subj.getNameID().getValue();
+		} else
+			return false;
+		
+		authenticatedId = input.getSubcontext(SubjectContext.class).getPrincipalName();
+		
+		//authenticatedId = getAuthID(request);
+		log.error("req: " + requestedId);
+		log.error("auth: " + authenticatedId);
+		if(authenticatedId.equals(requestedId))
+			return true;
+		else
+			return false;
+	}
+	
+}
